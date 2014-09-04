57c4f76b29e7fe4f677aa2fb5d294838a6fdeeaf
amazingSurge <amazingSurge@yahoo.com>
2014-09-04 05:40:05 -0700

    update

--- src/core.js
+++ src/core.js
@@ -405,14 +405,23 @@
     $.fn.asColorInput = function(options) {
         if (typeof options === 'string') {
             var method = options;
-            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+            var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : [];
 
-            return this.each(function() {
-                var api = $.data(this, 'asColorInput');
-                if (typeof api[method] === 'function') {
-                    api[method].apply(api, method_arguments);
+            if (/^\_/.test(method)) {
+                return false;
+            } else if ((/^(get)$/.test(method))) {
+                var api = this.first().data('asColorInput');
+                if (api && typeof api[method] === 'function') {
+                    return api[method].apply(api, method_arguments);
                 }
-            });
+            } else {
+                return this.each(function() {
+                    var api = $.data(this, 'asColorInput');
+                    if (api && typeof api[method] === 'function') {
+                        api[method].apply(api, method_arguments);
+                    }
+                });
+            }
         } else {
             return this.each(function() {
                 if (!$.data(this, 'asColorInput')) {
